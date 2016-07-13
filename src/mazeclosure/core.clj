7ab49bdeafe3f9bb15cfe29f4e15961d02d1345b
John Crooks <johnmcrooks@gmail.com>
2016-07-13 12:30:32 -0400

    finsihed maze generation

--- src/mazeclosure/core.clj
+++ src/mazeclosure/core.clj
@@ -10,8 +10,57 @@
         (for [col (range 0 size)]
           {:row row :col col :visited? false :bottom? true :right? true})))))
 
+(defn possible-neighbors [rooms row col]
+  (let [top-room (get-in rooms [(dec row) col])  ;decrease row to get room above current 
+        bottom-room (get-in rooms [(inc row) col])  ; increase row num to get room below current
+        left-room (get-in rooms [row (dec col)])
+        right-room (get-in rooms [row(inc col)])]
+    (filter 
+      (fn [room] 
+        (and (not (nil? room))             ;only keep rooms that aren't nil
+             (not (:visited? room))))     ; and those where the visited boolean aren't true
+      [top-room bottom-room left-room right-room])))
+
+
+(defn random-neighbor[rooms row col]
+  (let [neighbors (possible-neighbors rooms row col)]
+    (if (> (count neighbors) 0)  ;Making sure there are neighbors 
+      (rand-nth neighbors)    ;if greater than 0 grab a random neighbor from the set
+      nil)))                  ; otherwise do nothing
+
+(defn tear-down-wall [rooms old-row old-col new-row new-col]
+  (cond
+    (< new-row old-row)  ; Going up
+    (assoc-in rooms [new-row new-col :bottom?] false)
+    (> new-row old-row)  ; Going down
+    (assoc-in rooms [old-row old-col :bottom?] false)
+    (< new-col old-col)  ; Going Left
+    (assoc-in rooms [new-row new-col :right?] false)
+    (> new-col old-col)  ; Going right
+    (assoc-in rooms [old-row old-col :right?] false)))
+
+(declare create-maze)
+
+(defn create-maze-loop [rooms old-row old-col new-row new-col]
+  (let [new-rooms (tear-down-wall rooms old-row old-col new-row new-col)
+        new-rooms (create-maze new-rooms new-row new-col)]
+    (if (= rooms new-rooms)
+      rooms
+      (create-maze-loop new-rooms old-row old-col new-row new-col))))
+
+(defn create-maze [rooms row col]
+  (let [rooms (assoc-in rooms [row col :visited?] true)
+        next-room (random-neighbor rooms row col)]
+    (if next-room
+      (create-maze-loop rooms row col (:row next-room) (:col next-room))
+      rooms)))
+    
+      
+
+
 (defn -main []
-  (let [rooms (create-rooms)]  ; defining variable rooms))
+  (let [rooms (create-rooms)  ; defining variable rooms))
+        rooms (create-maze rooms 0 0)]
     (doseq [row rooms]         ;using this to print the top line of the grid
       (print " _"))
     (println)                  ;page break for teh ASCII art
